de41978c425b8eb9386a02a05f8fe9cc2fbc877a
ndethi <wndethi@gmail.com>
2018-09-10 23:48:02 +0300

    Prep Homepage Changes Initial

--- application/modules/Home/views/template/template_view.php
+++ application/modules/Home/views/template/template_view.php
@@ -35,7 +35,37 @@
                             <h2 class="head-title lg-line">Status in Kenya</h2>
                             <hr class="botm-line">
                         </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="col-sm-6 col-md-4">
+                        <div class="">
+                          <div class="text-center">
+                            <h1 class="large_numbers">1,600,000</h1>
+                            <hr>
+                            <p>Number of People with HIV (2016)</p>
+                          </div>
+                        </div>
                     </div>
+                    <div class="col-sm-6 col-md-4">
+                        <div class="">
+                          <div class="caption text-center">
+                            <h1 class="large_numbers">62,000</h1>
+                            <hr>
+                            <p>New Infections (2016)</p>
+                          </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <div class="">
+                          <div class="caption text-center">
+                            <h1 class="large_numbers">23,000</h1>
+                            <hr>
+                            <p>Prep Clients (2016)</p>
+                          </div>
+                        </div>
+                    </div>
+
+                </div>
                 <div class="col-md-12">
                     <div class="panel panel-info">
                         <div class="panel-heading">
@@ -189,6 +219,157 @@
         </script>
     </section>
     <!--about-->
+
+    <section id="resources" class="section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-4 col-xs-12">
+                    <div class="section-title">
+                        <h2 class="head-title lg-line">Gallery</h2>
+                        <hr class="botm-line">
+                    </div>
+                </div>
+          </div>
+
+          <div id="carousel-example-generic" class="col-md-12 carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+              <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+              <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+              <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+              <div class="item active">
+                <img src="https://picsum.photos/g/900/600/?random" class="img-responsive center-block" alt="...">
+                <div class="carousel-caption">
+                  <h5>First</h5>
+                  <p>First caption</p>
+                </div>
+              </div>
+              <div class="item">
+                <img src="https://picsum.photos/g/900/600/?random" class="img-responsive center-block" alt="...">
+                <div class="carousel-caption">
+                  <h5>Second Title</h5>
+                  <p>Second Caption</p>
+                </div>
+              </div>
+              <div class="item">
+                <img src="https://picsum.photos/g/900/600/?random" class="img-responsive center-block" alt="...">
+                <div class="carousel-caption">
+                  <h5>Third Title</h5>
+                  <p>Third Caption</p>
+                </div>
+              </div>
+            </div>
+
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+          &nbsp;
+          <div class="row">
+            <hr>
+            <div class="col-md-4">
+              <img src="https://picsum.photos/g/900/600/?random" class="img-responsive center-block" alt="..." class="img-thumbnail">
+              <h5>Gallery Title</h5>
+              <p><a href="#">View</a></p>
+            </div>
+            <div class="col-md-4">
+              <img src="https://picsum.photos/g/900/600/?random" class="img-responsive center-block" alt="..." class="img-thumbnail">
+              <h5>Gallery Title</h5>
+              <p><a href="#">View</a></p>
+            </div>
+            <div class="col-md-4">
+              <img src="https://picsum.photos/g/900/600/?random" class="img-responsive center-block" alt="..." class="img-thumbnail">
+              <h5>Gallery Title</h5>
+              <p><a href="#">View</a></p>
+            </div>
+
+          </div>
+        </div>
+    </section>
+
+    <section id="national_policies_guidelines" class="section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-4 col-xs-12">
+                    <div class="section-title">
+                        <h2 class="head-title lg-line">National Policies and Guidelines</h2>
+                        <hr class="botm-line">
+                    </div>
+                </div>
+            </div>
+            &nbsp;
+            <div class="row">
+              <div class="col-sm-6 col-md-4">
+                  <div class="thumbnail">
+                    <img src="https://picsum.photos/g/300/450/" alt="">
+                    <div class="caption">
+                      <h5>Prep Booklet V2 2018</h5>
+                      <p>Description of Prep Booklet V2 of 2018.</p>
+                      <p><a href="#">Download <i class="fa fa-download" aria-hidden="true"></i></a></p>
+                    </div>
+                  </div>
+              </div>
+        </div>
+    </section>
+
+    <section id="publications" class="section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-4 col-xs-12">
+                    <div class="section-title">
+                        <h2 class="head-title lg-line">Publications</h2>
+                        <hr class="botm-line">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+              <div class="input-group col-xs-12">
+                <label for="Year">Year</label>
+                <select class="" name="Year">
+                  <option value="">2018</option>
+                </select>
+                <label for="Topic">Topic</label>
+                <select class="" name="Topic">
+                  <option value="">Partners</option>
+                </select>
+              </div>
+            </div>
+            &nbsp;
+            <div class="row">
+              <div class="col-sm-6 col-md-4">
+                  <div class="thumbnail">
+                    <img src="https://picsum.photos/g/300/450/" alt="">
+                    <div class="caption">
+                      <h5>Prep Booklet V2 2018</h5>
+                      <p>Description of Prep Booklet V2 of 2018</p>
+                      <p><a href="#">Download <i class="fa fa-download" aria-hidden="true"></i></a></p>
+                    </div>
+                  </div>
+              </div>
+              <div class="col-sm-6 col-md-4">
+                  <div class="thumbnail">
+                    <img src="https://picsum.photos/g/300/450/" alt="">
+                    <div class="caption">
+                      <h5>Prep Booklet V3 2019</h5>
+                      <p>Description of Prep Booklet V3 of 2019</p>
+                      <p><a href="#">Download <i class="fa fa-download" aria-hidden="true"></i></a></p>
+                    </div>
+                  </div>
+              </div>
+            </div>
+        </div>
+    </section>
+
     <section id="faq" class="section-padding">
         <div class="container">
             <div class="row">
@@ -198,24 +379,36 @@
                         <hr class="botm-line">
                     </div>
                 </div>
-                <div class="col-md-9 col-sm-8 col-xs-12">
-                    <div style="visibility: visible;" class="col-sm-9 more-features-box">
-                        <div class="more-features-box-text">
-                            <div class="more-features-box-text-icon"> <i class="fa fa-angle-right" aria-hidden="true"></i> </div>
-                            <div class="more-features-box-text-description">
-                                <h3>Question One</h3>
-                                <p>Stub Answer One</p>
-                            </div>
-                        </div>
-                        <div class="more-features-box-text">
-                            <div class="more-features-box-text-icon"> <i class="fa fa-angle-right" aria-hidden="true"></i> </div>
-                            <div class="more-features-box-text-description">
-                                <h3>Question Two</h3>
-                                <p>Stub Answer Two</p>
-                            </div>
-                        </div>
-                    </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                  <img src="<?php echo base_url(); ?>public/home/img/prep_faqSheet_cover.png" alt="Prep FAQs" class="img-responsive">
+                  <div class="caption">
+                    <h5>Prep Booklet 2018 V2 - English </h5>
+                    <p>Description of Version 2 Prep Booklet of 2018 in English(GB)</p>
+                    <p><a href="#">Download <i class="fa fa-download" aria-hidden="true"></i></a></p>
+                  </div>
                 </div>
+            </div>
+            <div class="row">
+              <div class="col-md-9 col-sm-8 col-xs-12">
+                  <div style="visibility: visible;" class="col-sm-9 more-features-box">
+                      <div class="more-features-box-text">
+                          <div class="more-features-box-text-icon"> <i class="fa fa-angle-right" aria-hidden="true"></i> </div>
+                          <div class="more-features-box-text-description">
+                              <h3>Question One</h3>
+                              <p>Stub Answer One</p>
+                          </div>
+                      </div>
+                      <div class="more-features-box-text">
+                          <div class="more-features-box-text-icon"> <i class="fa fa-angle-right" aria-hidden="true"></i> </div>
+                          <div class="more-features-box-text-description">
+                              <h3>Question Two</h3>
+                              <p>Stub Answer Two</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
             </div>
         </div>
     </section>
